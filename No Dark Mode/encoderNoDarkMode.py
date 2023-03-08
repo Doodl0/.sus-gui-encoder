776e976c5704ee98ade3f5f7f4e3de3924154d92
Doodl0 <52604696+Doodl0@users.noreply.github.com>
2023-03-08 20:42:17 +0000

    Add files via upload

---
+++ No Dark Mode/encoderNoDarkMode.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from tkinter.filedialog import *
+import tkinter.ttk as ttk
+from random import randint
+
+char = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '\n']
+code = ""
+
+window = Tk()
+window.title("C Sus Sus GUI Compiler")
+window.columnconfigure([0],weight=0)
+window.columnconfigure([1],weight=1)
+window.rowconfigure([0],weight=1)
+
+text=Text()
+text.grid(row=0,column=1,sticky="nsew")
+scrollbarY=ttk.Scrollbar(orient='vertical',command=text.yview)
+scrollbarY.grid(row=0,column=0,sticky="nsew")
+text['yscrollcommand'] = scrollbarY.set
+
+def saveAsSus():
+    
+    code=text.get('1.0','end')
+
+    compiled = asksaveasfile(mode='w', defaultextension=".sus", filetypes=[('C Sus Sus Files', '*.sus')])
+    if compiled is None:
+        return
+    
+    for c in code:
+        if c in char:
+            compiled.write("s"*char.index(c))
+            compiled.write("\n")
+
+
+def saveAsPy():
+    
+    filecontent=text.get('1.0','end')
+
+    compiled = asksaveasfile(mode='w', defaultextension=".py", filetypes=[('Python Files', '*.py')])
+    if compiled is None: 
+        return
+
+    compiled.write(filecontent)
+
+def loadAsPy():
+    file = askopenfile(mode='r', defaultextension=".py", filetypes=[('Python Files', '*.py')])
+    if file is None: 
+        return
+
+    code = file.read()
+    text.delete("1.0",END)
+    text.insert("1.0",code)
+
+def loadAsSus():
+    file = askopenfile(mode='r', defaultextension=".sus", filetypes=[('C Sus Sus Files', '*.sus')])
+    if file is None: 
+        return
+
+    code = file.read()
+    text.delete("1.0",END)
+    text.insert("1.0",code)
+
+def loadSusAsPy():
+    file = askopenfile(mode='r', defaultextension=".sus", filetypes=[('C Sus Sus Files', '*.sus')])
+    if file is None: 
+        return
+
+    code = file.read()
+    index = 0
+    
+    text.delete("1.0",END)
+    for c in code:
+        if c == "s":
+            index += 1
+        elif c == "\n":
+            if index > len(char)-1:
+                index = len(char)-1
+            text.insert("end",char[index])
+            index = 0
+
+def newfile():
+    text.delete("1.0",END)
+
+mainmenu = Menu()
+filemenu = Menu(mainmenu, tearoff = 0)
+filemenu.add_command(label = "New File", command = newfile)
+filemenu.add_separator()
+filemenu.add_command(label = "Save as .sus", command = saveAsSus)
+filemenu.add_command(label = "Save as .py", command =  saveAsPy)
+filemenu.add_separator()
+filemenu.add_command(label = "Load .sus as .py", command = loadSusAsPy)
+filemenu.add_command(label = "Load .sus", command = loadAsSus)
+filemenu.add_command(label = "Load .py", command = loadAsPy)
+mainmenu.add_cascade(label="File", menu=filemenu)
+
+
+window.config(menu = mainmenu)
+
+window.mainloop()
